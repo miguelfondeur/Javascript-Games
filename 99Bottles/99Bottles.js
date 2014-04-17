429ca2034ff25115b6bb1f17ea69ecb649a1cca0
Miguel <mguelfondeur@gmail.com>
2014-04-16 23:57:29 -0400

    bottles

---
+++ 99Bottles/99Bottles.js
@@ -0,0 +1,11 @@
+//not really a game but you know...
+
+
+var start_num = 5;
+
+var num_now = start_num;
+
+while(num_now>100) {
+	console.log(num_now + ' bottles of beer on the wall, ' +  num_now  + ' bottles of beer!');
+	num_now--
+}
