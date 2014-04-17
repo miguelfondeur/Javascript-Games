0e80a7c9451cbbbbc8f8f8587bd32b0fd0643c8f
Miguel <mguelfondeur@gmail.com>
2014-04-17 00:27:26 -0400

    done

--- 99Bottles/99Bottles.js
+++ 99Bottles/99Bottles.js
@@ -1,11 +1,12 @@
 //not really a game but you know...
 
 
-var start_num = 5;
+var start_num = 99;
 
 var num_now = start_num;
 
-while(num_now>100) {
+while(num_now >0) {
 	console.log(num_now + ' bottles of beer on the wall, ' +  num_now  + ' bottles of beer!');
-	num_now--
+	num_now-- ;
+	console.log('Take one down, pass it around, ' + num_now + ' bottles of beer on the wall!');
 }
